2653e81fdc098a1488860041afef2d3ef657f645
motuto <ryuuma0425@gmail.com>
2020-03-20 19:14:11 +0900

    Delete comment

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,7 +65,6 @@ $(function(){
 
   var buildHTML = function(message) {
     if (message.content && message.image) {
-      //data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
         `<div class="upper-message">` +
           `<div class="upper-message__user-name">` +
@@ -83,7 +82,6 @@ $(function(){
         `</div>` +
       `</div>`
     } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
         `<div class="upper-message">` +
           `<div class="upper-message__user-name">` +
@@ -100,7 +98,6 @@ $(function(){
         `</div>` +
       `</div>`
     } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
         `<div class="upper-message">` +
           `<div class="upper-message__user-name">` +
@@ -119,27 +116,20 @@ $(function(){
   };
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.message:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       console.log(messages)
       if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.mainChat__message').append(insertHTML);
         $('.mainChat__message').animate({ scrollTop: $('.mainChat__message')[0].scrollHeight});
       }
